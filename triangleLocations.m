ca3165a4defb5b22332443340548a2a03b044f0c
Matteo Caoduro <matteo.caoduro@gmx.com>
2021-05-20 18:45:15 +0000

    Improved time performace

--- triangleLocations.m
+++ triangleLocations.m
@@ -1,19 +1,11 @@
 function [inside, border] = triangleLocations(T, VT, iXk)
-
-% This code is very slow!  It requires quadratic time on the size of T,
-% because it inspects all the triangles.  It can be performed in linear
-% time by inspecting only the local triangles.
-% 
+ 
 
 candidates = [VT{iXk}]';
 
 inside = candidates(all(ismember(T(candidates, :), iXk), 2));
 border = setdiff(candidates, inside);
 
-% candidates = find(any(ismember(T, iXk), 2));
-% 
-% inside = candidates(all(ismember(T(candidates, :), iXk), 2));
-% border = setdiff(candidates, inside);
 
 end
 
